b609d2c2988c62fd6f9a0cbaa3e5c1bd59f12bcf
arnav0418 <agupta5_be22@thapar.edu>
2026-01-09 16:20:37 +0530

    created routes for http endpoints

---
+++ ecomm-api/handler/routes.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+var r *chi.Mux
+
+func RegisterRoutes(handler *handler) *chi.Mux {
+	r = chi.NewRouter()
+
+	r.Route("/products", func(r chi.Router) {
+		r.Post("/", handler.createProduct)
+		r.Get("/", handler.listProducts)
+
+		r.Route("/{id}", func(r chi.Router){
+			r.Get("/", handler.getProduct)
+			r.Patch("/", handler.updateProduct)
+			r.Delete("/", handler.deleteProduct)
+		})
+	})
+
+	return r
+}
+
+func Start(addr string) error {
+	return http.ListenAndServe(addr, r)
+}
