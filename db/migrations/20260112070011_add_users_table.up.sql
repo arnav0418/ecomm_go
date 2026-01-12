5cc2e9e97931368ef4d89ff70d3365c0240c343c
arnav0418 <agupta5_be22@thapar.edu>
2026-01-12 15:46:43 +0530

    corrected sql query syntax

--- db/migrations/20260112070011_add_users_table.up.sql
+++ db/migrations/20260112070011_add_users_table.up.sql
@@ -1,9 +1,9 @@
-CREATE TABLE users(
-    'id' INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    'name' VARCHAR(255) NOT NULL,
-    'email' VARCHAR(255) NOT NULL UNIQUE,
-    'password' VARCHAR(255) NOT NULL,
-    'is_admin' BOOLEAN NOT NULL DEFAULT FALSE,
-    'created_at' datetime DEFAULT (now()),
-    'updated_at' datetime,
-)
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME
+);
