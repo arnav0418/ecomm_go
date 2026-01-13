78459887695321c114f99f907956e9f2610bdcc6
arnav0418 <agupta5_be22@thapar.edu>
2026-01-13 11:58:40 +0530

    added login res req struct mapping

--- ecomm-api/handler/types.go
+++ ecomm-api/handler/types.go
@@ -70,3 +70,17 @@ type UserRes struct {
 type ListUserRes struct {
 	Users []UserRes `json:"users"`
 }
+
+type LoginUserReq struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginUserRes struct {
+	SessionID             string    `json:"session_id"`
+	AccessToken           string    `json:"access_token"`
+	RefreshToken          string    `json:"refresh_token"`
+	AccessTokenExpiresAt  time.Time `json:"access_token_expires_at"`
+	RefreshTokenExpiresAt time.Time `json:"refresh_token_expires_at"`
+	User                  UserRes   `json:"user"`
+}
