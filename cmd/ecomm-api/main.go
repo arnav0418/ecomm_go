e7d062fbec4df2359857b715455561d0c467d53c
arnav0418 <agupta5_be22@thapar.edu>
2026-01-13 11:56:18 +0530

    defined secret key variable

--- cmd/ecomm-api/main.go
+++ cmd/ecomm-api/main.go
@@ -7,9 +7,17 @@ import (
 	"github.com/arnav0418/ecomm_go/ecomm-api/handler"
 	"github.com/arnav0418/ecomm_go/ecomm-api/server"
 	"github.com/arnav0418/ecomm_go/ecomm-api/storer"
+	"github.com/ianschenck/envflag"
 )
 
+const minSecretKeySize = 32
+
 func main() {
+	var secretKey = envflag.String("SECRET_KEY", "01234567890123456789012345678901", "secret key for jwt signing")
+	if len(*secretKey) < minSecretKeySize {
+		log.Fatalf("SECRET_KEY must be atleast %d characters", minSecretKeySize)
+	}
+
 	db, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("error opening database: %v", err)
@@ -20,7 +28,7 @@ func main() {
 	// do something with the database
 	st := storer.NewMySQLStorer(db.GetDB())
 	srv := server.NewServer(st)
-	hdl := handler.NewHandler(srv)
+	hdl := handler.NewHandler(srv, *secretKey)
 	handler.RegisterRoutes(hdl)
 	handler.Start(":8080")
 }
