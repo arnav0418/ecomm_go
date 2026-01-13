24c803458ad0aa02141d3b688acd287157cbb0d3
arnav0418 <agupta5_be22@thapar.edu>
2026-01-13 12:41:00 +0530

    added renew access token struct mappings

--- ecomm-api/handler/types.go
+++ ecomm-api/handler/types.go
@@ -84,3 +84,12 @@ type LoginUserRes struct {
 	RefreshTokenExpiresAt time.Time `json:"refresh_token_expires_at"`
 	User                  UserRes   `json:"user"`
 }
+
+type RenewAccessTokenReq struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type RenewAccessTokenRes struct {
+	AccessToken          string    `json:"access_token"`
+	AccessTokenExpiresAt time.Time `json:"access_token_expires_at"`
+}
